a6413d7890c931b77a7f07871457e6ab0e402f01
surrin1008 <qkrrmsals123@naver.com>
2021-05-12 20:04:29 +0900

    Java

---
+++ stringApp/src/stringA.java
@@ -0,0 +1,13 @@
+
+public class stringA {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println('H');
+		System.out.println("H");
+		System.out.println("Hello" + "World!");
+		System.out.println("Hello \nWorld!");
+		System.out.println("Hello \"World\"");
+	}
+
+}
